b467d876849346790acadffd756fb7ba2445df33
bernie-cm <bernie.camejo@icloud.com>
2025-01-28 17:53:33 +1000

    add more unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -16,13 +16,16 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a test node", TextType.ITALIC, "http://google.com")
         self.assertIsNotNone(node.url)
 
+    def test_url_not_eq(self):
+        node_1 = TextNode("Test node", TextType.BOLD, "http://amazon.com.au")
+        node_2 = TextNode("Test node", TextType.BOLD, "http://github.com")
+        self.assertNotEqual(node_1.url, node_2.url)
 
-"""
-Add even more test cases (at least 3 in total) to check various edge cases, 
-like when the url property is None, or when the text_type property is different. 
-You'll want to make sure that when properties are different, the TextNode 
-objects are not equal.
-"""
+    def test_two_attributes_not_eq(self):
+        node_1 = TextNode("Text node 1", TextType.IMAGE, "https://github.com")
+        node_2 = TextNode("Text node 2", TextType.BOLD, "htts://github.com")
+        self.assertNotEqual(node_1, node_2)
+        
 
 
 if __name__ == "__main__":
