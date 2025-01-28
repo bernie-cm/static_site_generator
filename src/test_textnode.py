4326424d3ac4370a5c334463ccdbd95da20d5977
bernie-cm <bernie.camejo@icloud.com>
2025-01-28 17:49:32 +1000

    add more unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -12,14 +12,12 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("Test node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
+    def test_empty_url(self):
+        node = TextNode("This is a test node", TextType.ITALIC, "http://google.com")
+        self.assertIsNotNone(node.url)
+
 
 """
-Add some test cases by adding methods to the TestTextNode class to verify that 
-the TextNode class works as expected. You can use the following methods to 
-compare the objects:
-self.assertEqual - if the inputs are equal the test passes
-self.assertNotEqual - if the inputs are not equal the test passes
-
 Add even more test cases (at least 3 in total) to check various edge cases, 
 like when the url property is None, or when the text_type property is different. 
 You'll want to make sure that when properties are different, the TextNode 
