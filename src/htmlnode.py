5d640ef8320af601a6e1482753c1fc994e074488
bernie-cm <bernie.camejo@icloud.com>
2025-01-29 19:28:01 +1000

    add third method to HTMLNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,5 +11,13 @@ class HTMLNode:
         Child classes will override this method to render themselves as HTML.
         """
         raise NotImplementedError
-
     
+    def props_to_html(self):
+        """Return a string that represents the HTML attributes of the node
+        For example, if self.props is
+        {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        Then self.props_to_html() should return href="https://www.google.com" target="blank"
+        """
