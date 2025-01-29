be1850ecf8ec77d1b64784a5e49dd7e863316ec1
bernie-cm <bernie.camejo@icloud.com>
2025-01-29 20:58:39 +1000

    start defining tests against HTMLNode class

---
+++ src/test_htmlnode.py
@@ -0,0 +1,8 @@
+import unittest
+from htmlnode import HTMLNode
+
+"""
+TODO: Create some tests for the HTMLNode class (at least 3). 
+TODO: I used a new file called src/test_htmlnode.py. 
+TODO: Create a few nodes and make sure the props_to_html method works as expected
+"""
