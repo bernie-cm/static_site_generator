e1badb5c3f78d2459a37703108e8bd7e6b665fb3
bernie-cm <bernie.camejo@icloud.com>
2025-01-31 15:49:02 +1000

    start writing method to convert text nodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -8,7 +7,6 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
-
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -25,3 +23,6 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    def text_node_to_html_node(text_node):
+        pass
