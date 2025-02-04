6f3aed31660a8f447908af82547cb8012309c342
bernie-cm <bernie.camejo@icloud.com>
2025-02-04 21:04:55 +1000

    Refactored textnode.py module

--- src/textnode.py
+++ src/textnode.py
@@ -23,3 +23,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextType):
+        raise Exception("text_node should be a valid type.")
+    
+    if text_node == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node)  # Raw text value with no tag
+    if text_node == TextType.BOLD:
+        pass
+    if text_node == TextType.ITALIC:
+        pass
+    if text_node == TextType.CODE:
+        pass
+    if text_node == TextType.LINK:
+        pass
+    if text_node == TextType.IMAGE:
+        pass
