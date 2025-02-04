11422e93646f29af47e22d87239886999165a0ae
bernie-cm <bernie.camejo@icloud.com>
2025-02-04 21:10:07 +1000

    Finished writing text_node_to_html function

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
+from htmlnode import LeafNode
 from enum import Enum
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -7,6 +9,7 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -23,21 +26,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
 
 def text_node_to_html_node(text_node):
-    if not isinstance(text_node, TextType):
-        raise Exception("text_node should be a valid type.")
-    
-    if text_node == TextType.TEXT:
-        return LeafNode(tag=None, value=text_node)  # Raw text value with no tag
-    if text_node == TextType.BOLD:
-        pass
-    if text_node == TextType.ITALIC:
-        pass
-    if text_node == TextType.CODE:
-        pass
-    if text_node == TextType.LINK:
-        pass
-    if text_node == TextType.IMAGE:
-        pass
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
